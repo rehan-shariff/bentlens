0cf676d53b2b507489b594c488ac13f9998c823f
Rehan Shariff <stackhalllearningservices@gmail.com>
2016-11-07 07:05:51 -0600

    fixed copyright in main

--- BentLens/main.m
+++ BentLens/main.m
@@ -3,13 +3,13 @@
 //  BentLens
 //
 //  Created by Rehan Shariff on 1/26/16.
-//  Copyright © 2016 StackHall. All rights reserved.
+//  Copyright © 2016 Stackhall Learning Services, LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
